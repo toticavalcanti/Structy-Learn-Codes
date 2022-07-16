f03cae4d362ebc7db7556b1b0f011840ed6e1cc0
toticavalcanti <toticavalcanti@hotmail.com>
2022-07-16 13:53:34 -0300

    insert challenge description in the comment

--- javascript/depthFirstTraversalRecursive.js
+++ javascript/depthFirstTraversalRecursive.js
@@ -1,4 +1,20 @@
+/*
+has path
 
+Write a function, hasPath, 
+that takes in an object representing the 
+adjacency list of a directed acyclic graph 
+and two nodes (src, dst). The function should 
+return a boolean indicating whether or not 
+there exists a directed path between the 
+source and destination nodes.
+
+Hey. This is our first graph problem, 
+so you should be liberal with watching 
+the Approach and Walkthrough. Be productive, 
+not stubborn. -AZ
+
+*/
 const depthFirstPrint = (graph, source) => {
 	console.log(source);
 	for(let neighbor of graph[source]){
