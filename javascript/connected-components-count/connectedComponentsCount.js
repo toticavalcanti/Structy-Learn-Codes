8088e72d05b742ad83ba0576c7d01268aaa029ec
toticavalcanti <toticavalcanti@hotmail.com>
2022-07-17 01:29:49 -0300

    finish connected components count

--- javascript/connected-components-count/connectedComponentsCount.js
+++ javascript/connected-components-count/connectedComponentsCount.js
@@ -1,5 +1,25 @@
-const connectedComponentsCount = (adjList) =>{
-	return 2;
-}
+const connectedComponentsCount = (graph) =>{
+	const visited = new Set();
+	let count = 0;
+	for(let node in graph){
+		if(explore(graph, node, visited)){
+			count += 1;
+		}
+	}
+
+	return count;
+};
+
+const explore = (graph, current, visited) => {
+	if(visited.has(String(current))) return false;
+
+	visited.add(String(current));
+
+	for(neighbor of graph[current]){
+		explore(graph, neighbor, visited);
+	}
+
+	return true;
+};
 
 module.exports = connectedComponentsCount;
--- javascript/connected-components-count/specConnectedComponentsCount.js
+++ javascript/connected-components-count/specConnectedComponentsCount.js
@@ -15,4 +15,53 @@ describe('Connected Components Count', function(){
 
 		assert.equal(connectedComponentsCount(adjList), 2)
 	})
+
+	it('Case 01', function(){
+		adjList = {
+			1: [2],
+			2: [1,8],
+			6: [7],
+			9: [8],
+			7: [6, 8],
+			8: [9, 7, 2]
+		};
+
+		assert.equal(connectedComponentsCount(adjList), 1)
+	})
+
+	it('Case 02', function(){
+		adjList = {
+			3: [],
+			4: [6],
+			6: [4, 5, 7, 8],
+			8: [6],
+			7: [6],
+			5: [6],
+			1: [2],
+			2: [1]
+		};
+
+		assert.equal(connectedComponentsCount(adjList), 3)
+	})
+
+	it('Case 03', function(){
+		adjList = {};
+
+		assert.equal(connectedComponentsCount(adjList), 0)
+	})
+
+	it('Case 04', function(){
+		adjList = {
+			0: [4,7],
+			1: [],
+			2: [],
+			3: [6],
+			4: [0],
+			6: [3],
+			7: [0],
+			8: []
+		};
+
+		assert.equal(connectedComponentsCount(adjList), 5)
+	})
 })
